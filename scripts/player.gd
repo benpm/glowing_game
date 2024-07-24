1e5e0aa47c073c76332efb9b646ef2ad673e6757
benpm <schwalegos@gmail.com>
2024-07-24 05:57:04 -0600

    just messin around

--- scripts/player.gd
+++ scripts/player.gd
@@ -3,7 +3,8 @@ class_name Player
 
 
 const SPEED = 300.0
-const JUMP_VELOCITY = -400.0
+const JUMP_VELOCITY = -100.0
+var jumpAmount = 5.0
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
@@ -14,12 +15,15 @@ func _physics_process(delta):
 		velocity.y += gravity * delta
 
 	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
+	if is_on_floor():
+		jumpAmount = 5.0
+	if Input.is_action_just_pressed("jump") and jumpAmount > 0.1:
+		velocity.y = JUMP_VELOCITY * jumpAmount
+		jumpAmount /= 2.0
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction = Input.get_axis("ui_left", "ui_right")
+	var direction = Input.get_axis("left", "right")
 	if direction:
 		velocity.x = direction * SPEED
 	else:
