6f029ae3be1c5c4fa15690cefef604abe4b3194e
benpm <schwalegos@gmail.com>
2024-07-24 01:14:45 -0600

    stuff2

--- scripts/game.gd
+++ scripts/game.gd
@@ -1,7 +1,25 @@
 extends Node
 
+@onready var player : Player = $/root/scene/player
+@onready var fx_material : CanvasItem = $fx_layer/display_fade_blur
+@onready var camera : Camera2D = $/root/scene/camera
 
-# Called when the node enters the scene tree for the first time.
+var last_viewport_pos: Vector2 = Vector2.ZERO
+
 func _ready():
-	RenderingServer.viewport_set_clear_mode(get_viewport().get_viewport_rid(), RenderingServer.VIEWPORT_CLEAR_NEVER)
+	assert(player, "Player node not found.")
+	assert(fx_material, "Shader material not found.")
+	assert(camera, "Camera node not found.")
 
+	# Disable the viewport clear mode to allow nice effect
+	RenderingServer.viewport_set_clear_mode(
+		get_viewport().get_viewport_rid(),
+		RenderingServer.VIEWPORT_CLEAR_ONLY_NEXT_FRAME
+	)
+
+func _process(_delta):
+	var cam_pos = camera.get_screen_center_position()
+	var delta_viewport_pos = last_viewport_pos - cam_pos
+	last_viewport_pos = cam_pos
+	fx_material.material.set_shader_parameter("camera_pos", cam_pos)
+	fx_material.material.set_shader_parameter("camera_delta", delta_viewport_pos)
--- scripts/player.gd
+++ scripts/player.gd
@@ -1,4 +1,5 @@
 extends CharacterBody2D
+class_name Player
 
 
 const SPEED = 300.0
@@ -6,7 +7,6 @@ const JUMP_VELOCITY = -400.0
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
-
 
 func _physics_process(delta):
 	# Add the gravity.
